b598ebbe4edcf3bcb7b0b908d184ce8d88361357
kenvinc <christian.kenvin@gmail.com>
2015-03-22 13:10:07 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,15 @@
 setwd("~/Documents/gettingandcleaningdata/runanalysis/")
 getwd()
-
-# Source of data for this project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-
 # merge 
 train <- read.table("train/X_train.txt")
 test <- read.table("test/X_test.txt")
 X <- rbind(train, test)
-train <- read.table("train/subject_train.txt")
-test <- read.table("test/subject_test.txt")
-L <- rbind(train, test)
 train <- read.table("train/y_train.txt")
 test <- read.table("test/y_test.txt")
 Y <- rbind(train, test)
-
+train <- read.table("train/subject_train.txt")
+test <- read.table("test/subject_test.txt")
+Z <- rbind(train, test)
 # extracts 
 features <- read.table("features.txt")
 goodfeatures <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
@@ -21,33 +17,29 @@ X <- X[, goodfeatures]
 names(X) <- features[goodfeatures, 2]
 names(X) <- gsub("\\(|\\)", "", names(X))
 names(X) <- tolower(names(X))
-
 # descriptive activity
 activitylabel <- read.table("activity_labels.txt")
 activitylabel[, 2] = gsub("_", "", tolower(as.character(activitylabel[, 2])))
 Y[,1] = activitylabel[Y[,1], 2]
 names(Y) <- "activities"
-
 # Labels
-names(L) <- "subject"
-merge <- cbind(L, Y, X)
+names(Z) <- "subject"
+merge <- cbind(Z, Y, X)
 write.table(merge, "mergeddata.txt")
-
 # averages sheet
-mysub = unique(L)[,1]
+mysub = unique(Z)[,1]
+col = dim(merge)[2]
 sub = length(unique(L)[,1])
 act = length(activitylabel[,1])
-col = dim(merge)[2]
-result = merge[1:(sub *act), ]
-
+answer = merge[1:(sub *act), ]
 row = 1
-for (y in 1:sub) {
-  for (x in 1:act) {
-    result[row, 1] = mysub[y]
-    result[row, 2] = activitylabel[x, 2]
-    template <- merge[merge$subject==y & merge$activities==activitylabel[x, 2], ]
-    result[row, 3:col] <- colMeans(template[, 3:col])
+for (i in 1:sub) {
+  for (n in 1:act) {
+    answer[row, 1] = mysub[i]
+    answer[row, 2] = activitylabel[n, 2]
+    template <- merge[merge$subject==i & merge$activities==activitylabel[n, 2], ]
+    answer[row, 3:col] <- colMeans(template[, 3:col])
     row = row+1
   }
 }
-write.table(result, "dataaverages.txt")
+write.table(answer, "dataaverages.txt")
