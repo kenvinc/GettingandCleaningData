8c22a645cf3d6439afde4e86f14b16ab3fa7e9fe
kenvinc <christian.kenvin@gmail.com>
2015-03-22 14:17:29 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,26 +4,24 @@ getwd()
 train <- read.table("train/X_train.txt")
 test <- read.table("test/X_test.txt")
 X <- rbind(train, test)
-train <- read.table("train/y_train.txt")
-test <- read.table("test/y_test.txt")
-Y <- rbind(train, test)
-train <- read.table("train/subject_train.txt")
-test <- read.table("test/subject_test.txt")
-Z <- rbind(train, test)
+train1 <- read.table("train/y_train.txt")
+test1 <- read.table("test/y_test.txt")
+Y <- rbind(train1, test1)
+train2 <- read.table("train/subject_train.txt")
+test2 <- read.table("test/subject_test.txt")
+Z <- rbind(train2, test2)
 # extracts 
 features <- read.table("features.txt")
-goodfeatures <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
-X <- X[, goodfeatures]
-names(X) <- features[goodfeatures, 2]
-names(X) <- gsub("\\(|\\)", "", names(X))
-names(X) <- tolower(names(X))
+aboutfeatures <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+X <- X[, aboutfeatures]
+names(X) <- features[aboutfeatures, 2]
 # descriptive activity
 activitylabel <- read.table("activity_labels.txt")
 activitylabel[, 2] = gsub("_", "", tolower(as.character(activitylabel[, 2])))
 Y[,1] = activitylabel[Y[,1], 2]
 names(Y) <- "activities"
-# Labels
-names(Z) <- "subject"
+# labels
+names(Z) <- "names"
 merge <- cbind(Z, Y, X)
 write.table(merge, "mergeddata.txt")
 # averages sheet
